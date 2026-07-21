9b568e0e335fda9b93b6bb059602babb8fac79fa
mudasir1134 <razamudasir1134@gmail.com>
2026-07-21 23:00:20 +0500

    Refractor the code

---
+++ app/chatbot.py
@@ -0,0 +1,41 @@
+from app.loader import DocumentLoader
+from app.chunker import TextChunker
+from app.embeddings import EmbeddingManager
+from app.vector_store import VectorStoreManager
+from app.retriever import Retriever
+from app.prompt_builder import PromptBuilder
+from app.llm import LLMManager
+
+class ChatBot:
+    def __init__(self):
+        print(f"Chatbot Initialized")
+
+        loader = DocumentLoader('Data\documents\sample.pdf')
+        documents = loader.load()
+
+        chunker = TextChunker()
+        chunks = chunker.split_documents(documents)
+
+        embedding_manager = EmbeddingManager()
+        embedding_model = embedding_manager.get_embedding_model()
+
+        vector_manager = VectorStoreManager(embedding_model)
+        vector_store = vector_manager.create_vector_store(chunks)
+    
+        self.retriever = Retriever(vector_store)
+    
+        self.prompt_builder = PromptBuilder()
+
+        self.llm = LLMManager()
+
+        print("Chat bot Ready!")
+        
+    def ask(self , question:str)->str:
+        retrieved_docs = self.retriever.retrieve(question)
+        prompt = self.prompt_builder.build_prompt(
+            question,
+            retrieved_docs
+        )
+        answer = self.llm.generate_response(prompt)
+        return answer
+    
--- app/main.py
+++ app/main.py
@@ -1,46 +1,19 @@
-from app.loader import DocumentLoader
-from app.chunker import TextChunker
-from app.embeddings import EmbeddingManager
-from app.vector_store import VectorStoreManager
-from app.retriever import Retriever
-from app.prompt_builder import PromptBuilder
-from app.llm import LLMManager
+from app.chatbot import ChatBot
 
 def main():
-    loader = DocumentLoader('Data\documents\sample.pdf')
-    documents = loader.load()
+    chatbot =ChatBot()
 
-    chunker = TextChunker()
-    chunks = chunker.split_documents(documents)
-
-    embedding_manager = EmbeddingManager()
-    embedding_model = embedding_manager.get_embedding_model()
-
-    vector_manager = VectorStoreManager(embedding_model)
-    vector_store = vector_manager.create_vector_store(chunks)
-    
-    retriever = Retriever(vector_store)
-    
-    prompt_builder = PromptBuilder()
-
-    llm = LLMManager()
-
-    print("Simple RAG based system")
-    print("Type 'EXIT' to quit")
+    print("A simple RAG Based Chatbot")
+    print("Type exit to quit")
 
     while True:
-        question = input("\nYou: ")
-        if question.lower()=="exit":
-            break
-        retrieved_docs = retriever.retrieve(question)
-        prompt = prompt_builder.build_prompt(
-            question,
-            retrieved_docs
-        )
-        answer = llm.generate_response(prompt)
-        print(f"\nBot: {answer}")
+        question = input("Ask: ")
 
-    
+        if question.lower() == "exit":
+            print("Good Byeee!")
+            break
+        answer = chatbot.ask(question)
+        print(f"Bot Says : {answer}")
 
 if __name__ == "__main__":
     main()
